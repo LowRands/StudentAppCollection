600091837a74394baa565a29bcc1e5fc2d30ae15
EthanLourens-STUDENT <G00467054@atu.ie>
2025-10-08 16:58:27 +0100

    Student details

--- src/main/java/ie/atu/Week4/StudentApp.java
+++ src/main/java/ie/atu/Week4/StudentApp.java
@@ -1,8 +1,17 @@
-package ie.atu.week4;
+package ie.atu.Week4;
 import java.util.Scanner;
 
 public class StudentApp {
     public static void main(String[] args) {
+        int count = 0;
         Scanner input = new Scanner(System.in);
-
+        System.out.print("Please enter number of students: ");
+        int TotalStudents = input.nextInt();
+        System.out.print("Please enter your name: ");
+        String name = input.next();
+        System.out.print("Please enter your email: ");
+        String email = input.next();
+        System.out.print("Enter Student ID: ");
+        int studentID = input.nextInt();
     }
+}
