6b726511c0b13d087492faf1e420bdc4bac1bf3a
EthanLourens-STUDENT <G00467054@atu.ie>
2025-10-08 16:54:15 +0100

    Start of project

--- src/main/java/ie/atu/Week4/StudentApp.java
+++ src/main/java/ie/atu/Week4/StudentApp.java
@@ -1,4 +1,8 @@
-package ie.atu.Week4;
+package ie.atu.week4;
+import java.util.Scanner;
 
 public class StudentApp {
-}
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    }
